465828505f3cc427a984925a2e694c4f88ac6d67
zoey-yjj <zoeyyyjj@gmail.com>
2022-12-02 10:43:17 +0800

    bind socket address to given socket

--- tcp_server.cpp
+++ tcp_server.cpp
@@ -46,6 +46,13 @@ namespace http
             exitWithError("Cannot create socket");
             return 1;
         }
+
+        if (bind(m_socket, (sockaddr *)&m_socketAddress, m_socketAddress_len) < 0)
+        {
+            exitWithError("Cannot connect socket to address");
+            return 1;
+        }
+
         return 0;
     }
 
