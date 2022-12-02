db304314a025ea2cb1bbb3c94595ba9d069762d3
zoey-yjj <zoeyyyjj@gmail.com>
2022-12-02 09:31:46 +0800

    add log

--- tcp_server.cpp
+++ tcp_server.cpp
@@ -1,14 +1,42 @@
 #include <tcp_server.h>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
+namespace
+{
+    void log(const std::string &message)
+    {
+        std::cout << message << std::endl;
+    }
+
+    void exitWithError(const std::string &errorMessage)
+    {
+        log("ERROR: " + errorMessage);
+        exit(1);
+    }
+}
 
 namespace http
 {
-    TcpServer::TcpServer()
+    TcpServer::TcpServer(std::string ip_address, int port) : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(),
+                                                             m_incomingMessage(),
+                                                             m_socketAddress(), m_socketAddress_len(sizeof(m_socketAddress)),
+                                                             m_serverMessage(buildResponse())
     {
+        m_socketAddress.sin_family = AF_INET;
+        m_socketAddress.sin_port = htons(m_port);
+        m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
+        {
+            startServer();
+        }
     }
 
     TcpServer::~TcpServer()
     {
+        closeServer();
     }
+
 
     int TcpServer::startServer()
     {
--- tcp_server.h
+++ tcp_server.h
@@ -17,7 +17,20 @@ namespace http
         void startListen();
 
     private:
+        std::string m_ip_address;
+        int m_port;
+        int m_socket;
+        int m_new_socket;
+        long m_incomingMessage;
+        struct sockaddr_in m_socketAddress;
+        unsigned int m_socketAddress_len;
+        std::string m_serverMessage;
 
+        int startServer();
+        void closeServer();
+        void acceptConnection(int &new_socket);
+        std::string buildResponse();
+        void sendResponse();
     };
 } 
 
